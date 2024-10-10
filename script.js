e26c09bd3779eabaf79b7ee906e850c060e8ed97
Muneer Girnari <muneeragirnari@gmail.com>
2024-10-10 12:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let element = document.getElementbyId("output");
+
+let promise = new Promise ((resolve) => {
+	setTimeout(() => {
+		resolve("Hello, world!")
+	},1000);
+});
+
+promise.then((data) =>{
+	element.innerText = data;
+});
